89f68ad3db0e972f0c5dadbc810e37a897de9084
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2024-12-14 11:14:31 +0000

    Removed unused property

--- src/main/java/imigration/api/service/EmailService.java
+++ src/main/java/imigration/api/service/EmailService.java
@@ -1,31 +1,19 @@
 package imigration.api.service;
 
-import java.time.Instant;
-
-import org.springframework.http.HttpStatusCode;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.server.ResponseStatusException;
-
-import imigration.api.model.entity.User;
-import imigration.api.model.entity.VerificationToken;
-import imigration.api.repository.VerificationTokenRepository;
 
 @Service
 public class EmailService {
 
     private final JavaMailSender javaMailSender;
-    private final VerificationTokenRepository verificationTokenRepository;
-
+    
     public EmailService(
-        final JavaMailSender javaMailSender, 
-        final VerificationTokenRepository verificationTokenRepository
+        final JavaMailSender javaMailSender
     ) {
         this.javaMailSender = javaMailSender;
-        this.verificationTokenRepository = verificationTokenRepository;
     }
 
     @Async
