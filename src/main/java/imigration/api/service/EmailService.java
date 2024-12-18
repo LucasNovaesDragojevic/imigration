79fc22f79cce1910974afd47731aeb957537e509
Lucas Novaes Dragojevic <lucas.dragojevic@gmail.com>
2024-12-18 19:31:04 +0000

    Added logs to send email

--- src/main/java/imigration/api/service/EmailService.java
+++ src/main/java/imigration/api/service/EmailService.java
@@ -1,5 +1,7 @@
 package imigration.api.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(EmailService.class);
     private final JavaMailSender javaMailSender;
     
     public EmailService(
@@ -18,19 +21,23 @@ public class EmailService {
 
     @Async
     public void sendVerification(final String email, final String token) {
+        LOGGER.info("Sending verification e-mail.");
         final var message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("Verify your e-mail");
         message.setText("http://localhost:8080/email-verification/" + token);
         javaMailSender.send(message);
+        LOGGER.info("Sent verification e-mail.");
     }
 
     @Async
     public void sendRecovery(final String email, final String token) {
+        LOGGER.info("Sending recovery e-mail.");
         final var message = new SimpleMailMessage();
         message.setTo(email);
         message.setSubject("Recovery password");
         message.setText("http://localhost:8080/passwords/reset/" + token);
         javaMailSender.send(message);
+        LOGGER.info("Sent recovery e-mail.");
     }
 }
